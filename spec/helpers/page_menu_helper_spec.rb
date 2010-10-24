06c6aa793dd47d31b2e617949ff8d6875024a79e
Jim Gay <jim@saturnflyer.com>
2010-10-23 23:12:26 -0400

    use the let method

--- spec/helpers/page_menu_helper_spec.rb
+++ spec/helpers/page_menu_helper_spec.rb
@@ -13,36 +13,36 @@ describe PageMenuExtension::PageMenuHelper do
 
   describe "#child_link" do
     it "should disable a link with no options" do
-      @page.stub!(:allowed_children).and_return([])
-      child_link_for(@page).should match('<span class="action disabled">')
+      page.stub!(:allowed_children).and_return([])
+      child_link_for(page).should match('<span class="action disabled">')
     end
 
     it "should link to Page#new with a single option" do
-      @page.stub!(:allowed_children).and_return([Page])
-      child_link_for(@page).should match(Regexp.escape(new_admin_page_child_path(@page, :page_class => 'Page')))
+      page.stub!(:allowed_children).and_return([Page])
+      child_link_for(page).should match(Regexp.escape(new_admin_page_child_path(page, :page_class => 'Page')))
     end
 
     it "should link to div with multiple options" do
-      @page.stub!(:allowed_children).and_return([Page,FileNotFoundPage])
-      child_link_for(@page).should match("#allowed_children_#{page_id(:home)}")
+      page.stub!(:allowed_children).and_return([Page,FileNotFoundPage])
+      child_link_for(page).should match("#allowed_children_#{page_id(:home)}")
     end
   end
 
   describe "#children_for" do
     it "should not show virtual pages to designers" do
-      @page.stub!(:allowed_children).and_return([Page, ArchivePage, FileNotFoundPage])
+      page.stub!(:allowed_children).and_return([Page, ArchivePage, FileNotFoundPage])
       current_user.stub!(:admin?).and_return(false)
-      children_for(@page).flatten.should_not include(FileNotFoundPage)
+      children_for(page).flatten.should_not include(FileNotFoundPage)
     end
   end
   
   describe '#clean_page_description' do
     it "should remove all whitespace (except single spaces) from the given page's description" do
-      @page.stub!(:description).and_return(%{
+      page.stub!(:description).and_return(%{
         This is the  description   for the   
             current page!
       })
-      clean_page_description(@page).should == 'This is the description for the current page!'
+      clean_page_description(page).should == 'This is the description for the current page!'
     end
   end
 end
